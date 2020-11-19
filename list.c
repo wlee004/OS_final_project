e3506378ec6d9f8a13740c402c50c7fb449afff6
MuradAle <65867559+MuradAle@users.noreply.github.com>
2020-11-19 14:20:16 -0500

    Update list.c

--- list.c
+++ list.c
@@ -9,27 +9,51 @@
 #include <unistd.h>
 
 int main(void){
-	chdir("./Dir0");
-	// creating child by using fork()
-	int child_1, child_2;
-	// creating child by using fork()
-	
-	child_1 = fork();
-	if(child_1 == 0){
-		char *argv1[] = {"clear",NULL};
+    pid_t child_1, child_2; // create all process id
+    
+    int mypipe[2];
+    pipe(mypipe);
+
+    chdir("./Dir0"); // Changes Directory
+
+    child_1 = fork();
+    if (child_1 < 0)
+    {
+        perror("[ERROR] child_1");
+        return 1;
+    }
+    else if (child_1 == 0)
+    {
+        // no need for pipe
+        close(mypipe[0]);
+        close(mypipe[1]);
+
+        char *argv1[] = {"clear",NULL};
 		execvp(argv1[0], argv1);
+    }
+    else{
+		wait(0);
+	}
+
+	child_2 = fork();
+	if (child_2 < 0)
+	{
+		perror("[ERROR] child_2");
+		return 1;
+	}
+	else if (child_2 == 0)
+	{
+		close(mypipe[0]);
+		char *argv1[] = {"ls","-la",NULL};
+		execvp(argv1[0], argv1);
+		close(mypipe[1]);
 	}
 	else{
-		child_2 = fork();
-		if(child_2 == 0){
-			char *argv1[] = {"ls","-la",NULL};
-			execvp(argv1[0], argv1);
-		}
+		wait(0);
 	}
-	
+	//As the system() call uses a shell to execute the command
+	system("ls -la > t1.txt");
 	int ret = rename("t1.txt", "tree.txt");
 	
-	// wait for the childrens to end 
-	wait(0);
-	wait(0);
+    return 0;
 }
