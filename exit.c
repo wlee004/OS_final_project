c4de84ba749419a10e44e2147d55af8d95d70c65
MCardenas38 <41306780+MCardenas38@users.noreply.github.com>
2020-11-21 20:38:27 +0000

    Update exit.c

--- exit.c
+++ exit.c
@@ -10,34 +10,35 @@
 
 int main(void){
 
-    int child1;   
-    int child2;
-
-    if((child1= fork()) == -1){
+    if((child1= fork()) == -1){ //Forks first child
         printf("ERROR: First Child \n");
         return -1;
     }
     
-    if(child1==0){
-		execlp("ls","ls","-l",NULL);
-        printf("Ls FAILED \n");
-        exit(0);
+    if(child1==0){//First Child outputs last 4 commands to terminal
+       printf("History");
+       execlp("tail","tail", "-4", "history",NULL);
+       printf("history Failed!");
+       exit(0);
+		
     }       
     else{
-        if((child2= fork()) == -1){
+        if((child2= fork()) == -1){//Forks second child
             printf("ERROR: Second Child \n");
             return -1;
         }
 
-        if(child2==0){
-           execlp("/bin/sh","sh","-c","tail -4 ~/.bash_history",NULL);
-           printf("history Failed!");
-           exit(0);
+        if(child2==0){//Second Child performs ls on current directory
+            execlp("ls","ls","-l",NULL);
+            printf("Ls FAILED \n");
+            exit(0);
         }
         else{
             int status;
             waitpid(child1,&status,0);
             waitpid(child2,&status,0);
+            
+            //Parent waits for both child processes to finish then waits for user to press enter to exit
 			printf("\nPress [ENTER] to exit....");
             getchar();
             exit(0);
@@ -46,4 +47,3 @@ int main(void){
    
     return 0;
 }
-
